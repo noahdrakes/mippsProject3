94d0e7398021b4279257d8c6c8004a59fd098c6b
noahdrakes <noah.drakes@bison.howard.edu>
2022-04-24 19:25:58 -0400

    tested stack, to see if byte of string was being accessed

--- project3.s
+++ project3.s
@@ -40,35 +40,49 @@ main:
 
         #   loads user input address to stack pointer
         lw $t0, 0($sp)
-        #   store 4 bytes to stack pointer
+        #   restore 4 bytes to stack pointer
         addi $sp, $sp, 4
         
+        lb $t8, 0($t0)
 
-
+        
 
         #   loop to process entire string
-
         li $t1, 0
-
         loop:
-            beq $t1, 1000, exitProgram
+            beq $t1, $zero, exitProgram
             
 
+            #                                   #
+            #           PARSE STRING            #
+            #                                   #
+            loop1:
+                lb $t2, 0($t0)
+                beq $t2, 59, loop
 
 
+            j loop1 
             
 
             j sub_b
 
 
-            # converts
-            sub_b:
+            
 
         addi $t1, $t1, 1
         j loop
 
-
+    # converts integers to valid characters
+        sub_b:
 
     exitProgram:
         li $v0, 10
         syscall
+
+
+
+
+
+        # print register testing , putting at the bottom for 
+        # debugging purposes
+        
